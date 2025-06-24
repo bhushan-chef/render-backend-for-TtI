d8167eb52395b809d3469deec181b40fb04b9832
bhushan-chef <153006192+bhushan-chef@users.noreply.github.com>
2025-06-25 00:53:10 +0530

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
-import connectDB from './config/mongodb.js';
-import userRouter from './routes/userRoutes.js';
-import imageRouter from './routes/imageRouter.js';
-const PORT = process.env.PORT
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRouter.js";
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-await connectDB()
-app.use('/api/user',userRouter)
-app.use('/api/image',imageRouter)
-app.get('/',(req,res)=>res.send("API Working"))
-app.listen(PORT,()=>console.log("Server running on Port"+PORT));
+await connectDB();
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+app.get("/", (req, res) => res.send("API Working"));
+app.listen(PORT, () => console.log("Server running on Port" + PORT));
